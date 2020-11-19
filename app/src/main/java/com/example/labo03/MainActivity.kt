75821e8b049d5bff3666d5fa98fe709cc63414b4
jerome <jerome.arn@heig-vd.ch>
2020-11-19 18:47:06 +0100

    template

---
+++ app/src/main/java/com/example/labo03/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.labo03
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var iBeacons     : Button
+    private lateinit var codeBarres   : Button
+    private lateinit var nfc          : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        iBeacons = findViewById(R.id.iBeacons)
+        codeBarres = findViewById(R.id.CodesBarres)
+        nfc = findViewById(R.id.NFC)
+
+
+        iBeacons.setOnClickListener{
+            intent = Intent(this, Ibeacons::class.java)
+            startActivity(intent)
+        }
+
+        codeBarres.setOnClickListener{
+            intent = Intent(this, Nfc::class.java)
+            startActivity(intent)
+        }
+
+        nfc.setOnClickListener{
+            intent = Intent(this, codeBarre::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/labo03/codeBarre.kt
@@ -0,0 +1,11 @@
+package com.example.labo03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class codeBarre : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_barre)
+    }
+}

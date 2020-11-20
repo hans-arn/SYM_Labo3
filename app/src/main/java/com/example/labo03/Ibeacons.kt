4a51dce48e88deb5fb44dffa7665472d1d39b296
petitRastignac <vincentcanipel@gmail.com>
2020-11-20 20:50:18 +0100

    important lib beacon

--- app/src/main/java/com/example/labo03/Ibeacons.kt
+++ app/src/main/java/com/example/labo03/Ibeacons.kt
@@ -1,5 +1,6 @@
 package com.example.labo03
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class Ibeacons : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ibeacons)
+
+
     }
 }
